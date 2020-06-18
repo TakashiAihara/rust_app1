388965b03dabe3a1c294a14aeb8e04fb7b2ca82b
takashi <takashi.aihara.0523@gmail.com>
2020-06-18 09:07:06 +0900

    add communicator

---
+++ communicator/target/debug/deps/communicator-0ad94540b1054392.d
@@ -0,0 +1,10 @@
+/Users/takashi/.ghq_src/github.com/TakashiAihara/rust_app1/communicator/target/debug/deps/communicator-0ad94540b1054392.rmeta: src/lib.rs src/client.rs src/network.rs src/network/server.rs
+
+/Users/takashi/.ghq_src/github.com/TakashiAihara/rust_app1/communicator/target/debug/deps/libcommunicator-0ad94540b1054392.rlib: src/lib.rs src/client.rs src/network.rs src/network/server.rs
+
+/Users/takashi/.ghq_src/github.com/TakashiAihara/rust_app1/communicator/target/debug/deps/communicator-0ad94540b1054392.d: src/lib.rs src/client.rs src/network.rs src/network/server.rs
+
+src/lib.rs:
+src/client.rs:
+src/network.rs:
+src/network/server.rs:
